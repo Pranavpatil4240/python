2a7475412dcbab79e47e732ba299114ed6eb7ede
pranav patil <151824314+Pranavpatil4240@users.noreply.github.com>
2024-08-08 18:07:35 +0530

    Add files via upload

---
+++ operation.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#pass something retun something and perform arithematic operations
+
+
+# In[1]:
+
+
+#addition
+
+def add (a,b):
+    res=a+b
+    return res
+add(2,4)
+  
+
+
+# In[2]:
+
+
+#substraction
+def sub (a,b):
+    res=a-b
+    return res
+sub(2,4)
+  
+
+
+# In[3]:
+
+
+#miltiplication
+
+def mul (a,b):
+    res=a*b
+    return res
+mul(2,4)
+  
+
+
+# In[4]:
+
+
+#division
+def div(a,b):
+    res=a/b
+    return res
+div(2,4)
+  
+
+
+# In[5]:
+
+
+# modulus
+
+def mod (a,b):
+    res=a%b
+    return res
+mod(2,4)
+  
+
+
+# In[6]:
+
+
+#dfloor division
+def fl (a,b):
+    res=a//b
+    return res
+fl(8,4)
+
+
+# In[7]:
+
+
+#area of circle
+def aoc (r):
+    res=3.14*r*r
+    return res
+aoc(5)
+
+
+# In[8]:
+
+
+# area of square
+def aos (a):
+    res=a*a
+    return res
+aos(2)
+
+
+# In[9]:
+
+
+#area of rectangle
+def aor (l,w):
+    res=l*w
+    return res
+aor(2,4)
+
+
+# In[13]:
+
+
+#area of triangle
+def aot (b,h):
+    res=1/2*b*h
+    return res
+aot(5,6)
+
+
+# In[11]:
+
+
+#area of rectangle
+def aor (l,w):
+    res=l*w
+    return res
+aor(2,4)
+
+
+# In[14]:
+
+
+#squre
+def sqr(a):
+    res=a*a
+    return res
+sqr(4)
+
+
+# In[15]:
+
+
+#squareroot
+def sq(a):
+    res=a**0.5
+    return res
+sq(5)
+
+
+# In[ ]:
+
+
+
+
